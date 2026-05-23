ac5e7501595a29755c747a0877916c392654cd36
Muhammed Baran insel <baraninsel0@gmail.com>
2026-05-24 02:09:59 +0300

    Allow all origins for Socket.IO CORS

--- server/src/index.js
+++ server/src/index.js
@@ -29,7 +29,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
